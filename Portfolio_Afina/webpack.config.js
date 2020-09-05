86fd93e5830e1225f0e81d61c933b2df4181a2cc
Nur Afina <finanurafina96@gmail.com>
2020-09-05 15:14:12 +0700

    changes

--- Portfolio_Afina/webpack.config.js
+++ Portfolio_Afina/webpack.config.js
@@ -19,6 +19,19 @@ module.exports = {
                         loader: "css-loader"
                     }
                 ]
+            },
+            /* babel loader */
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use : [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                ]
             }
         ]
     }
